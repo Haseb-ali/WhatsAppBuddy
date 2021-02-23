b9e9c597eefef025578cbe8a39d55c32d21f18aa
HASEEB <engineer.haseb@gmail.com>
2021-02-23 22:22:03 +0500

    validating login form

---
+++ static/js/loginvalidation.js
@@ -0,0 +1,28 @@
+let loginForm=document.querySelector("#loginForm");    
+let fieldsArray=[loginForm[1],loginForm[2]];
+const validateFrom=()=>{
+    let validateCount=0;    
+    fieldsArray.map((field)=>{
+        console.log(field.value);
+        if(field.value.length==0){
+            field.classList.add("make-border-denger");
+            validateCount++;
+        }
+    })
+    if(validateCount>0){
+        return false;
+    }
+               
+}
+fieldsArray.map((field)=>{
+    field.addEventListener("focus",(e)=>{
+        e.target.classList.remove("make-border-denger");
+    })
+})
+fieldsArray.map((field)=>{
+    field.addEventListener("blur",(e)=>{
+        if(e.target.value.trim().length==0){
+            e.target.classList.add("make-border-denger");
+        }
+    })
+})
